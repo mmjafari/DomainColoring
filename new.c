fe64dfcf147cd40ea407fe785cdb3a148ef9ec8d
mmjafari <mmjafari1381@gmail.com>
2021-05-08 16:02:54 +0900

    fixed orientation

--- new.c
+++ new.c
@@ -1,6 +1,6 @@
-#define z (k + j*I)/512			// define the complex variable, devide or multiply by real value to zoom or by imaginary value to rotate and zoom, add or subtract value to scroll 
+#define z (k - j*I)/64			// define the complex variable, devide or multiply by real value to zoom or by imaginary value to rotate and zoom, add or subtract value to scroll 
 #define CDP (12 * log(pow(256,5)))	// color depth. Adjust by the Expression
-#define EXP clog(z) 			// the Expression to be plotted. Must be described using complex.h functions, or other complex valued functions.
+#define EXP cpow(z,15) - 100*I 			// the Expression to be plotted. Must be described using complex.h functions, or other complex valued functions.
 #define RED (10 * log(cabs(EXP)))	// Red, adjusted absolute value of Expression
 #define GRN (fabs(carg(EXP)) * CDP * 10)// Green, drawing lines to describe isomorphism
 #define BLU fabs((GRN * M_PI / RED))	// Blue, decorative
@@ -12,12 +12,10 @@
 int main() {
   int j,k;
   printf("P3 2049 2049 %f\n", CDP);		// matrix size, color depth, header for NetPPM
-  for ( j = 1024; j >= -1024; j--){	        //-1024 to 1024 describes a 2049x2049 grid for the plot. if changed, ppm header must be changed accordingly
-    for ( k = -1024; k <= 1024; k++){
+  for ( j = -1024; j <= 1024; j++){	        //-1024 to 1024 describes a 2049x2049 grid for the plot. if changed, ppm header must be changed accordingly
+    for ( k = 1024; k >= -1024; k--){
       printf("%f %f %f\n", RED, GRN, BLU);
     }
   }
-  free(k);
-  free(j);
   return 0;
 }
