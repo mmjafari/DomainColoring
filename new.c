6b28fb82d7257a14bf3420d1fd53aacd8ef8ecd2
mmjafari <mmjafari1381@gmail.com>
2021-06-17 15:02:27 +0900

    updated the thing

--- new.c
+++ new.c
@@ -1,19 +1,23 @@
-#define z (k - j*I)/64			// define the complex variable, devide or multiply by real value to zoom or by imaginary value to rotate and zoom, add or subtract value to scroll 
-#define CDP (12 * log(pow(256,5)))	// color depth. Adjust by the Expression
-#define EXP cpow(z,15) - 100*I 			// the Expression to be plotted. Must be described using complex.h functions, or other complex valued functions.
-#define RED (10 * log(cabs(EXP)))	// Red, adjusted absolute value of Expression
-#define GRN (fabs(carg(EXP)) * CDP * 10)// Green, drawing lines to describe isomorphism
+#define CDP (12 * log(pow(256,5)))  	// color depth. Adjust by the Expression
+#define EXP cgamma(z / 16)  	     	// the Expression to be plotted. Must be described using complex.h functions, or other complex valued functions.
+#define RED (10 * log(cabs(iter)))   	// Red, adjusted absolute value of Expression
+#define GRN (fabs(carg(iter)) * CDP * 10)// Green, drawing lines to describe differentiability
 #define BLU fabs((GRN * M_PI / RED))	// Blue, decorative
 
 #include<stdio.h>
+#include"../RiemannES.h"
 #include<math.h>
 #include<complex.h>
 
 int main() {
-  int j,k;
-  printf("P3 2049 2049 %f\n", CDP);		// matrix size, color depth, header for NetPPM
-  for ( j = -1024; j <= 1024; j++){	        //-1024 to 1024 describes a 2049x2049 grid for the plot. if changed, ppm header must be changed accordingly
-    for ( k = 1024; k >= -1024; k--){
+  int x,y;
+  float complex z;
+  long double complex iter;
+  printf("P3 513 513 %f\n", CDP);		// matrix size, color depth, header for NetPPM
+  for ( y= -256; y <= 256; y++){	    //-1024 to 1024 describes a 2049x2049 grid for the plot. if changed, ppm header must be changed accordingly
+    for ( x = -256; x <= 256; x++){
+      z = (x - y*I);
+      iter = EXP;
       printf("%f %f %f\n", RED, GRN, BLU);
     }
   }
